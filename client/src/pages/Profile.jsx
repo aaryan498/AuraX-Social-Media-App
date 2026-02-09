b09a991e2852621b8a9119b0c110f45f7b3b4512
aaryan498 <kumaraaryan498@gmail.com>
2026-02-09 12:00:26 +0530

    Started with profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+
+
+
+  const { profileId } = useParams()
+  const [user, setuser] = useState(null)
+  const [posts, setposts] = useState([])
+  const [activeTab, setactiveTab] = useState('posts')
+  const [showEdit, setshowEdit] = useState('false')
+
+
+
+  const fetchUser = async () => {
+    setuser(dummyUserData)
+    setposts(dummyPostsData)
+  }
+
+  useEffect(()=>{
+    fetchUser()
+  },[])
+
+
+
+  return user ? (
+    <div className='relative h-full overflow-y-scroll bg-gray-50 p-6'>
+      <div className='max-w-3xl mx-auto'>
+        {/* Profile Card */}
+        
+
+      </div>
+    </div>
+  ) : <Loading/>
 }
 
 export default Profile
