334179d866dd0274c093fae6027891cc61668225
Aaryan Kumar <kumaraaryan498@gmail.com>
2026-02-28 00:03:35 +0530

    Cron bug fixed in inngest function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -139,7 +139,7 @@ const deleteStory = inngest.createFunction(
 
 const sendNotificationOfUnseenMessages = inngest.createFunction(
     {id: 'send-unseen-messages-notification'},
-    {cron: 'TZ=America/New York 0 9 * * *'},      //  Every day at 9 am
+    {cron: 'TZ=America/New_York 0 9 * * *'},      //  Every day at 9 am
     async({ step })=>{
         const messages = await Message.find({seen: false}).populate('to_user_id');
         const unseenCount = {}
